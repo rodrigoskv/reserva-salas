8c1437edac61dfc2e94277e7d6117a1ad42cce76
rodrigoskv <166267162+rodrigoskv@users.noreply.github.com>
2025-06-30 14:29:43 -0300

    Update CreateRoomsTable.php

--- app/Database/Migrations/CreateRoomsTable.php
+++ app/Database/Migrations/CreateRoomsTable.php
@@ -9,8 +9,8 @@ class CreateRoomsTable extends Migration
     public function up()
     {
         $this->forge->addField([
-            'id' => ['type' => 'INT', 'auto_increment' => true],
-            'name' => ['type' => 'VARCHAR', 'constraint' => 255],
+            'id'       => ['type' => 'INT', 'auto_increment' => true],
+            'name'     => ['type' => 'VARCHAR', 'constraint' => 255],
             'capacity' => ['type' => 'INT'],
         ]);
         $this->forge->addKey('id', true);
