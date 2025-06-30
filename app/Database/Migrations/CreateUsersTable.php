a9f4c6cdda595b582d68c9e7d5fb330d0efe5957
rodrigoskv <166267162+rodrigoskv@users.noreply.github.com>
2025-06-30 14:27:07 -0300

    Update CreateUsersTable.php

--- app/Database/Migrations/CreateUsersTable.php
+++ app/Database/Migrations/CreateUsersTable.php
@@ -9,10 +9,11 @@ class CreateUsersTable extends Migration
     public function up()
     {
         $this->forge->addField([
-            'id' => ['type' => 'INT', 'auto_increment' => true],
-            'name' => ['type' => 'VARCHAR', 'constraint' => 255],
-            'email' => ['type' => 'VARCHAR', 'constraint' => 255, 'unique' => true],
+            'id'       => ['type' => 'INT', 'auto_increment' => true],
+            'name'     => ['type' => 'VARCHAR', 'constraint' => 255],
+            'email'    => ['type' => 'VARCHAR', 'constraint' => 255, 'unique' => true],
             'password' => ['type' => 'VARCHAR', 'constraint' => 255],
+            'role'     => ['type' => 'VARCHAR', 'constraint' => 20, 'default' => 'user'],
         ]);
         $this->forge->addKey('id', true);
         $this->forge->createTable('users');
