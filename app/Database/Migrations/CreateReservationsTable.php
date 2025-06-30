dbc03fc7d42ef7ed97feb74ebe5fcd3356728f05
rodrigoskv <166267162+rodrigoskv@users.noreply.github.com>
2025-06-30 14:30:00 -0300

    Update CreateReservationsTable.php

--- app/Database/Migrations/CreateReservationsTable.php
+++ app/Database/Migrations/CreateReservationsTable.php
@@ -9,14 +9,13 @@ class CreateReservationsTable extends Migration
     public function up()
     {
         $this->forge->addField([
-            'id' => ['type' => 'INT', 'auto_increment' => true],
-            'user_id' => ['type' => 'INT'],
-            'room_id' => ['type' => 'INT'],
-            'date' => ['type' => 'DATE'],
+            'id'         => ['type' => 'INT', 'auto_increment' => true],
+            'user_id'    => ['type' => 'INT'],
+            'room_id'    => ['type' => 'INT'],
+            'date'       => ['type' => 'DATE'],
             'start_time' => ['type' => 'TIME'],
-            'end_time' => ['type' => 'TIME'],
+            'end_time'   => ['type' => 'TIME'],
         ]);
-
         $this->forge->addKey('id', true);
         $this->forge->addForeignKey('user_id', 'users', 'id', 'CASCADE', 'CASCADE');
         $this->forge->addForeignKey('room_id', 'rooms', 'id', 'CASCADE', 'CASCADE');
